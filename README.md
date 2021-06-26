e23a5d5f11e65b9af116b74f956e60e54c94743b
berry3 <berry3@indiana.edu>
2021-06-26 12:03:34 -0400

    last commit before archiving, closes #17

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # ligature-lab
 A front-end application for experimenting with Ligature and related technologies.
 
+## MOVED
+
+This project isn't being work on anymore and has been moved to [ligature-js](https://github.com/almibe/ligature-js).
+
 ## Running
 ligature-lab requires npm to run, see https://www.npmjs.com/ for setup.
 When running currently you probably want to use `npm run dev` and then go to `localhost:3000`.
--- src/components/dataset/LigatureDataset.svelte
+++ src/components/dataset/LigatureDataset.svelte
@@ -2,6 +2,18 @@
     import type { Dataset } from "../../store/store";
 
     export let dataset: Dataset
+
+    function addStatement() {
+        //TODO
+    }
+
+    
 </script>
 
-Ligature - {dataset.name} - {dataset.url}
+<div class="row">
+    <div class="col-sm"><button type="button" class="btn btn-outline-dark" on:click={() => addStatement()}>Add Statement</button></div>
+</div>
+<div class="row">
+    <div class="col-sm"><button type="button" class="btn btn-outline-dark" on:click={() => editDataset(dataset)}>Add Statement</button></div>
+    <div class="col-sm"><button type="button" class="btn btn-outline-dark" on:click={() => editDataset(dataset)}>Add Statement</button></div>
+</div>
