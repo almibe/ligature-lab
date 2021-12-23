b05a884c3d7993edc5dc0eb6eb7291cebee06ce4
berry3 <berry3@indiana.edu>
2021-12-22 23:43:14 -0500

    starting to add modals for Dataset management, #22

--- src/lib/LigatureInstance.svelte
+++ src/lib/LigatureInstance.svelte
@@ -1,51 +1,106 @@
 <script>
-    //import { run } from '@ligature/ligature'
-    import { run } from "../../../ligature-scala/ligature-js/js/target/scala-3.1.0/ligature-js-fastopt/main.js"
-  
-  
-    let script = ""
-    let output = ""
-  
-    function runScript() {
-      output = run(script)
+	import { createEventDispatcher, onMount } from 'svelte'
+	const dispatch = createEventDispatcher()
+
+    export let datasets = []
+
+    let removeDatasetModal
+
+    onMount(async () => {
+        let bs = await import("../../node_modules/bootstrap/dist/js/bootstrap.esm")
+        removeDatasetModal = new bs.Modal(document.getElementById('removeDatasetModal'), {})
+    })
+
+    function addDataset(name) {
+        //TODO show modal
+        //TODO if okay then publish addDataset event
     }
-  
-    function addDataset() {
-      //TODO show modal
-      //TODO if okay then publish addDataset event
+
+    function removeDataset(name) {
+        removeDatasetModal.show()
+        //TODO show modal
+        //TODO if okay then publish removeDataset event
     }
-  </script>
+
+    function refreshDatasets() {
+        dispatch('refreshDatasets')
+    }
+</script>
   
-  <div class="container">
+<div class="container">
     <div class="row">
-      <h1>Ligature Lab</h1>
+        <div class="col">
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addDatasetModal">
+                Add Dataset
+            </button>            
+        </div>  
+        <div class="col">
+            <button on:click={refreshDatasets} type="button" class="btn btn-primary float-end">Refresh</button>
+        </div>
     </div>
-  </div>
-  
-  <div class="container">
-    <div class="row">
-      <div class="col">
-        <button on:click={addDataset} type="button" class="btn btn-primary">Add Dataset</button>
-      </div>  
-      <div class="col">
-        <button on:click={runScript} type="button" class="btn btn-primary float-end">Refresh</button>
-      </div>
+
+    <div class="container">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Dataset Name</th>
+                    <th scope="col">Remove?</th>
+                </tr>
+            </thead>
+            <tbody>
+                {#each datasets as dataset}
+                    <tr>
+                        <td>
+                            {dataset}
+                        </td>
+                        <td>
+                            <button type="button" class="btn btn-secondary" on:click={e => removeDataset(dataset)}>Remove</button>
+                        </td>
+                    </tr>
+                {/each}
+            </tbody>
+        </table>
     </div>
-  </div>
-  
-  <div class="container">
-    <div class="row">
-      <textarea rows="15" bind:value={script}></textarea>
+</div>
+
+<div class="modal fade" id="addDatasetModal" tabindex="-1" aria-labelledby="addDatasetModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="addDatasetModalLabel">Add Dataset</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
     </div>
-  </div>
-  
-  <div class="container">
-    <div class="row">
-      <textarea readonly rows="10" bind:value={output}></textarea>
+</div>  
+
+<div class="modal fade" id="removeDatasetModal" tabindex="-1" aria-labelledby="removeDatasetModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="removeDatasetModalLabel">Remove Dataset</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
     </div>
-  </div>
-  
-  <style>
-      @import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
-  </style>
-  
+</div>  
+
+
+
+<style>
+    @import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
     import LigatureInstance from '$lib/LigatureInstance.svelte'
     import LigatureDataset from '$lib/LigatureDataset.svelte'
+
+    let testDatasets = ["hello", "world"]
+
+
 </script>
 
-<LigatureInstance></LigatureInstance>
+<LigatureInstance datasets={testDatasets} on:refreshDatasets={event => alert("Refresh Datasets")}></LigatureInstance>
 
 <hr />
 
