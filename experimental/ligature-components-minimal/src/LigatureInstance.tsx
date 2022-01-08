d21edad129c6eda2889f29afdd60ff153884b3ed
berry3 <berry3@indiana.edu>
2022-01-08 05:40:06 -0500

    improving add dataset support, #32

--- experimental/ligature-components-minimal/src/LigatureInstance.tsx
+++ experimental/ligature-components-minimal/src/LigatureInstance.tsx
@@ -1,12 +1,33 @@
 import '../node_modules/blueprint-css/dist/blueprint.css'
 import '../node_modules/@shoelace-style/shoelace/dist/themes/light.css'
 import '../node_modules/@shoelace-style/shoelace/dist/components/button/button.js'
+import '../node_modules/@shoelace-style/shoelace/dist/components/input/input.js'
+import '../node_modules/@shoelace-style/shoelace/dist/components/dialog/dialog.js'
 import './ligature.css'
+import { createEffect, createSignal, onMount } from 'solid-js'
 
-function LigatureInstance(props: any) {
+type LigatureInstanceCompanion = {
+  datasets: () => string[]
+  addDataset: (dataset: string) => Promise<void>
+  removeDataset: (dataset: string) => Promise<void>
+  selectDataset: (dataset: string) => Promise<void>
+}
+
+function LigatureInstance(props: LigatureInstanceCompanion) {
+  onMount(async () => {
+    const dialog = document.querySelector('#addDatasetDialog')
+    const addDatasetButton = document.querySelector('#addDatasetButton')
+    const input = dialog.querySelector('#newDatasetName')
+    addDatasetButton.addEventListener('click', () => dialog.show())
+    dialog.addEventListener('sl-initial-focus', event => {
+      event.preventDefault()
+      input.focus({ preventScroll: true })
+    })
+  })
+
   return <>
     <div bp="container">
-        <sl-button bp="float-left" variant="primary" outline>Add Dataset</sl-button>
+        <sl-button bp="float-left" variant="primary" outline id="addDatasetButton">Add Dataset</sl-button>
         <sl-button bp="float-right" variant="primary" outline>Refresh</sl-button>
         <div bp="clear-fix"></div>
     </div>
@@ -19,16 +40,46 @@ function LigatureInstance(props: any) {
           </tr>
         </thead>
         <tbody>
-          <For each={props.datasets}>{(dataset) =>
-            <tr>
-              <td>{dataset}</td>
-              <td><sl-button variant="danger" outline>Remove</sl-button></td>
-            </tr>
+          <For each={props.datasets()}>{(dataset) =>
+            <DatasetRow dataset={dataset}></DatasetRow>
           }</For>
         </tbody>
       </table>
     </div>
+
+    <sl-dialog label="Add Dataset" id="addDatasetDialog" style="--width: 50vw;">
+      <sl-input id="newDatasetName" label="Dataset Name"></sl-input>
+      <div slot="footer">
+        <sl-button variant="primary" bp="margin-right">Add</sl-button>
+        <sl-button variant="danger">Cancel</sl-button>
+      </div>
+    </sl-dialog>
+
+    <sl-dialog label="Remove Dataset" id="removeDatasetDialog" style="--width: 50vw;">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      <div slot="footer">
+        <sl-button variant="primary" bp="margin-right">Remove</sl-button>
+        <sl-button variant="primary">Cancel</sl-button>
+      </div>
+    </sl-dialog>
   </>
+}
+
+function DatasetRow(props: any) {
+  function remove() {
+    console.log("remove")
+  }
+
+  onMount(async () => {
+    const dialog = document.querySelector('#removeDatasetDialog')
+    const removeDatasetButton = document.querySelector("#removeDataset" + props.dataset)
+    removeDatasetButton.addEventListener('click', () => dialog.show())
+  })
+
+  return <tr>
+    <td>{props.dataset}</td>
+    <td><sl-button variant="danger" outline id={"removeDataset" + props.dataset} onClick={() => remove()}>Remove</sl-button></td>
+  </tr>
 }
 
 export default LigatureInstance
--- experimental/ligature-components-minimal/src/index.tsx
+++ experimental/ligature-components-minimal/src/index.tsx
@@ -1,9 +1,15 @@
+import { createSignal } from 'solid-js'
 import { render } from 'solid-js/web'
 import LigatureInstance from './LigatureInstance'
 
+const [datasets, setDatasets] = createSignal(["hello", "world"])
+
 render(() =>
   <>
-    <LigatureInstance datasets={["hello", "test"]} />
+    <LigatureInstance 
+      datasets={datasets}
+      addDataset={async (dataset: string) => {console.log("adding ", dataset); return unit} }
+      removeDataset={async (dataset: string) => {console.log("removing ", dataset); return unit} } />
   </>,
   document.getElementById('root') as HTMLElement
 )
