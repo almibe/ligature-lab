653e129af524b190d87b6135535f478b496bd40a
berry3 <berry3@indiana.edu>
2022-01-14 17:25:18 -0500

    adding additional data to package.json + including css in dist, #38

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import LigatureDataset from "./LigatureDataset";
 import LigatureInstance from "./LigatureInstance";
+import './ligature.css';
 
 export {LigatureInstance};
 export {LigatureDataset};
