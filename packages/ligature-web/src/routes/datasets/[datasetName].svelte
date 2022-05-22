5c51018fd12fd485dd6daf04f9aa79fca52ec559
berry3 <berry3@indiana.edu>
2022-05-22 03:25:44 -0400

    basic tabby support on dataset page

--- packages/ligature-web/src/routes/datasets/[datasetName].svelte
+++ packages/ligature-web/src/routes/datasets/[datasetName].svelte
@@ -1,1 +1,31 @@
-<h1>Dataset</h1>
+<script>
+import "blueprint-css/dist/blueprint.css";
+import "tabbyjs/dist/css/tabby-ui.css";
+import { page } from '$app/stores';
+import { onMount } from 'svelte';
+
+onMount(async () => {
+    const Tabby = (await import('tabbyjs')).default
+    let tabs = new Tabby('[data-tabs]');
+});
+</script>
+
+<h1>Dataset {$page.params.datasetName}</h1>
+
+<ul data-tabs>
+    <li><a data-tabby-default href="#query">Query</a></li>
+    <li><a href="#insert">Insert</a></li>
+    <li><a href="#remove">Remove</a></li>
+</ul>
+
+<div id="query">
+    Query
+</div>
+
+<div id="insert">
+    Insert
+</div>
+
+<div id="remove">
+    Remove
+</div>
