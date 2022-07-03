eea0bc3c9d3e42d940d20522f9774d5eab7c7663
Alex Michael Berry <alexmiberry@gmail.com>
2022-07-03 19:13:40 -0400

    removing old code (solidjs/old ui)

--- packages/ligature-components/demo.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from 'solid-js/web'
-import { LigatureEditor } from './src/index'
-
-render(() =>
-  <>
-    <LigatureEditor></LigatureEditor>   
-  </>,
-  document.getElementById('root') as HTMLElement
-)
--- packages/ligature-components/src/index.ts
+++
@@ -1,8 +0,0 @@
-import LigatureDataset from "./LigatureDataset"
-import LigatureInstance from "./LigatureInstance"
-import LigatureEditor from "./LigatureEditor"
-import "./ligature-components.css"
-
-export { LigatureInstance }
-export { LigatureDataset }
-export { LigatureEditor }
