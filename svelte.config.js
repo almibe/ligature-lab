233dc932478383801b710ec561ab2a4fe61da214
Alex Michael Berry <alexmiberry@gmail.com>
2022-07-06 02:35:34 -0400

    adding dotenv-defaults for config, #48

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,10 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
+//not sure if this is the best place for this, but it works
+import dotenv from "dotenv-defaults"
+dotenv.config()
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
