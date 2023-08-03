e6d56ad94d3e9cde7290ad299bb80e493fb3cbe8
Alex Michael Berry <alexmiberry@gmail.com>
2023-08-02 20:13:12 -0400

    using shoelace, #86 using codemirror clean up of old code

--- ligature-components/src/ligature-editor.ts
+++ ligature-components/src/ligature-editor.ts
@@ -1,20 +1,77 @@
-import { LitElement, css, html } from 'lit'
-import { customElement, property } from 'lit/decorators.js'
+import { LitElement, css, html } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+import './ligature-result.ts';
+import '@shoelace-style/shoelace/dist/shoelace.js';
+import {EditorView, basicSetup} from "codemirror";
 
 @customElement('ligature-editor')
 export class LigatureEditor extends LitElement {
+  private inputEditor: EditorView;
+  private runEvent;
+  private lastResult: any;
+
+  firstUpdated(): void {
+    const editorNode = this.shadowRoot.getElementById("editor")!!;
+    this.inputEditor = new EditorView({
+        extensions: [
+            EditorView.domEventHandlers({
+                keydown: e => {
+                    if((e.code == "Enter") && (e.metaKey || e.ctrlKey)) {
+                        e.preventDefault();
+                    }
+                }
+            }),
+            basicSetup, 
+        ],
+        parent: editorNode
+    });
+    this.inputEditor.focus();
+
+    this.shadowRoot.getElementById("resultDisplay").addEventListener("sl-change", (e) => {
+      this._updateResult();
+    })
+  }
+
+  connectedCallback(): void {
+    super.connectedCallback();
+    this.runEvent = (e) => {
+      if((e.code == "Enter") && (e.metaKey || e.ctrlKey)) {
+          this._run();
+      }
+    };
+    this.runEvent = document.body.addEventListener('keydown', this.runEvent);
+  }
+
+  disconnectedCallback(): void {
+    super.disconnectedCallback();
+    document.body.removeEventListener('keydown', this.runEvent);
+  }
 
   render() {
     return html`
       <div id="container">
-        <button @click="${this._run}">Run</button>
-        <textarea id="editor-area"></textarea>
-        <textarea id="result-area" readonly="readonly"></textarea>
+        <span id="controls">
+          <sl-button @click="${this._run}">Run</sl-button>
+
+          <sl-select id="resultDisplay" value="json">
+            <sl-option value="json">JSON</sl-option>
+            <sl-option value="text">Text</sl-option>
+            <sl-option value="table">Table</sl-option>
+            <sl-option value="graph">Graph</sl-option>
+          </sl-select>
+        </span>
+        <div id="editor"></div>
+        <ligature-result id="result"></ligature-result>
       </div>
     `;
   }
 
   static styles = css`
+    #controls {
+      display: flex;
+      flex-direction: row;
+    }
+
     #container {
       display: flex;
       flex-direction: column;
@@ -29,17 +86,50 @@ export class LigatureEditor extends LitElement {
     #result-area {
       flex: 1;
     }
+
+    #editor {
+      height:300px;
+      resize: vertical;
+      overflow:scroll;
+  }
   `;
 
-  private async _run(e: Event) {
-    let script = this.shadowRoot.getElementById("editor-area").value;
+  private _updateResult() {
+    let displayType: "text" | "json" = this.shadowRoot.getElementById("resultDisplay").value;
+    if (displayType == "text") {
+      this.shadowRoot.getElementById("result").resultText = this._scriptResultToText();
+    } else {
+      this.shadowRoot.getElementById("result").resultText = JSON.stringify(this.lastResult, null, 2);
+    }
+  }
+
+  private _scriptResultToText(): String {
+    if (this.lastResult["Ok"]) {
+      if (this.lastResult["Ok"]["Int"]) {
+        return this.lastResult["Ok"]["Int"];
+      } else if (this.lastResult["Ok"]["String"]) {
+        return this.lastResult["Ok"]["String"];
+      } else if (this.lastResult["Ok"]["Identifier"]) {
+        return "<" + this.lastResult["Ok"]["Identifier"] + ">";
+      } else if (this.lastResult["Ok"]["List"]) {
+        return "[...]";
+      } else {
+        return this.lastResult["Ok"];
+      }
+    } else {
+      return "Error: " + this.lastResult;
+    }
+  }
+
+  private async _run() {
+    let script = this.inputEditor.state.doc.toString();
     console.log(script);
     if (script != null && script != undefined) {
-      let result = JSON.parse(await (await fetch("/wander", {
+      this.lastResult = JSON.parse(await (await fetch("/wander", {
         method: "POST",
         body: script
       })).text());
-      this.shadowRoot.getElementById("result-area").value = JSON.stringify(result, null, 2);
+      this._updateResult();
     }
   }
 }
---
+++ ligature-components/src/ligature-result.ts
@@ -0,0 +1,27 @@
+import { LitElement, css, html } from 'lit'
+import { customElement, property } from 'lit/decorators.js'
+
+@customElement('ligature-result')
+export class LigatureResult extends LitElement {
+
+  @property()
+  resultText: String = ""
+
+  render() {
+    return html`
+        <div class="resultText"><pre>${this.resultText}</pre></div>
+    `;
+  }
+
+  static styles = css`
+    .resultText {
+        padding-top: 2em;
+    }
+    `;
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'ligature-result': LigatureResult
+  }
+}
