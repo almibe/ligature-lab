d7e2c2c7717c97dc7cc43545d2a8f6da95a19fc9
Alex Michael Berry <alexmiberry@gmail.com>
2023-08-11 20:38:53 -0400

    adding proxy for lig

--- ligature-components/vite.config.ts
+++ ligature-components/vite.config.ts
@@ -3,7 +3,8 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     server: {
         proxy: {
-            "/wander": "http://127.0.0.1:4200"
+            "/wander": "http://127.0.0.1:4200",
+            "/lig": "http://127.0.0.1:4200",
         }
     }
 });
