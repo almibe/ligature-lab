d0afb43e4123e6b2a50d4d066f34c65e76c6fe7b
Alex Michael Berry <alexmiberry@gmail.com>
2023-08-19 17:45:08 -0400

    getting project building again, closes #95

--- ligature-lab/astro.config.mjs
+++ ligature-lab/astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
-import svelte from '@astrojs/svelte';
 import solid from '@astrojs/solid-js';
 
 // https://astro.build/config
 export default defineConfig({
     output: 'server',
-    integrations: [svelte(), solid()]
+    integrations: [solid()]
 });
