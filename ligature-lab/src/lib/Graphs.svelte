c94b5ff44de3f6a79679ada4374025bd8650846e
Alex Michael Berry <alexmiberry@gmail.com>
2024-02-25 13:11:07 -0500

    removing bits-ui adding shoelace, adding fonts + fixing style, #106

---
+++ ligature-lab/src/lib/Graphs.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+import AddGraph from "./AddGraph.svelte";
+</script>
+
+<div id="graphs" class="content">
+    <h2 class="title is-3 is-inline">Graphs</h2>
+    <AddGraph />
+    <!-- <button id="addGraph" class="button" on:click={addGraph}>Add Graph</button> -->
+
+    <ul>
+        <li><a href="/graphs/sample">Sample Graph</a></li>
+    </ul>
+</div>
