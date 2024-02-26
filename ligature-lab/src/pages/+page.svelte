9c10aa6b42c6c6e78c200ae578bc3b74628a9c6f
Alex Michael Berry <alexmiberry@gmail.com>
2024-02-26 00:04:41 -0500

    starting to remove sveltekit project and replace it with astro one, #110

--- ligature-lab/src/pages/+page.svelte
+++ ligature-lab/src/pages/+page.svelte
@@ -4,13 +4,15 @@
     import Graphs from '$lib/Graphs.svelte';
     import { onMount } from 'svelte';
     import '../main.css';
+    import '@shoelace-style/shoelace/dist/themes/light.css';
+    import '@shoelace-style/shoelace/dist/components/button/button.js';
     onMount(async () => {
 
     })
 </script>
 
 <header>
-    <h1 class="title is-2" style="display:inline">Ligature Lab</h1>
+    <h1>Ligature Lab</h1>
 
     {#if $page.data.session?.user}
         <p>Hello, {$page.data.session.user.email} &dash; <button on:click={signOut} id="signOut">Sign out</button></p>
