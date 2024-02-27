9de715b8316e6b00d3ffa150f52d429650d465ce
Alex Michael Berry <alexmiberry@gmail.com>
2024-02-26 20:47:54 -0500

    starting to use lit and shoelace in ui, #111

--- ligature-lab/astro.config.mjs
+++ ligature-lab/astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import auth from "auth-astro";
-
 import node from "@astrojs/node";
+
+import lit from "@astrojs/lit";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [auth()],
+  integrations: [auth(), lit()],
   output: "server",
   adapter: node({
     mode: "standalone"
