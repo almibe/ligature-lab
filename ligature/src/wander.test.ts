75aac534438514226a168796307cb99d34e1b575
Alex Michael Berry <alexmiberry@gmail.com>
2024-08-10 23:32:29 -0400

    removing ligature + wander implementations

--- ligature/src/wander.test.ts
+++
@@ -1,8 +0,0 @@
-import { expect, test } from 'vitest'
-import { parse } from './wander'
-import { inMemoryNetwork } from './in-memory-network'
-
-test('test tokenization', () => {
-    expect(parse("{}")).toEqual([inMemoryNetwork()])
-    expect(parse("hello")).toEqual([{word: "hello", arguments: []}])
-})
--- wander/README.md
+++
@@ -1,9 +0,0 @@
-# Wander
-
-## Build
-
-- Build: `pnpm run res:build`
-- Clean: `pnpm run res:clean`
-- Build & watch: `pnpm run res:dev`
-- Test: `pnpm test`
-- Run Demo: `pnpm dev`
