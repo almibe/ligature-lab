7753cd0aa7595ad6213e80a0f0b586fcc2998a8a
Alex Michael Berry <alexmiberry@gmail.com>
2024-08-20 01:56:35 -0400

    moving around old code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,53 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { newEngine } from "@ligature/ligature";
+  import { browser } from '$app/environment';
+  import { wsConnect } from '$lib'
+
+	let text: string = '';
+  let result: string = "@ {}";
+  let engine = newEngine()
+
+  let wsEngine: undefined | any = undefined
+
+	function checkInput(event) {
+		if (event.keyCode == 13) {
+      wsEngine.run(text)
+      // result = JSON.stringify(engine.run(text))
+      text = ""
+      event.preventDefault();
+    }
+	}
+
+  if (browser) {
+    wsEngine = wsConnect("ws://localhost:8080/websocket")
+    wsEngine.addListener((cbResult) => result = cbResult)
+
+    document.querySelector("#input").focus();
+  }
+</script>
+
+<div id="container">
+  <div id="input">
+    <textarea  bind:value={text} on:keydown={checkInput}></textarea>  
+  </div>
+  <section id="results">{result}</section>
+</div>
+
+<style>
+  #container {
+  }
+
+  #results {
+    padding: 20px;
+  }
+
+  #input {
+  }
+
+  textarea {
+    resize: vertical;
+    field-sizing: content;
+    width: 100%;
+    height:1;
+  }
+</style>
