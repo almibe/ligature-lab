101e722576d9dbe3704cc2f9e44b40e1ee376a45
Alex Michael Berry <alexmiberry@gmail.com>
2024-09-03 02:05:09 -0400

    fixing printing out results

--- src/lib/components/TextView.svelte
+++ src/lib/components/TextView.svelte
@@ -4,7 +4,9 @@
     let result = ""
 
     let printValue = (value) => {
-        if (value.identifier) {
+        if (!value) {
+            return "--"
+        } else if (value.identifier) {
             return value.identifier
         } else if (value.slot) {
             return "$" + value.slot
@@ -12,23 +14,46 @@
             return "@" + value.networkName
         } else if (value.int) {
             return value.int
-        } else if (value.string) {
+        } else if (typeof value.string === "string") {
             return JSON.stringify(value.string)
-        } else {
-            return "Unset"
+        } else if (value.network) {
+            return printNetwork(value)
+        } else if (value.quote) {
+            return printQuote(value)
+        }else {
+            return "--"
         }
     }
 
-    engine.addListener((state, partialResult) => {
+    let printQuote = (quote) => {
+        console.log(quote)
+        let result = "["
+        quote.quote.forEach(value => {
+            result = result + " " + printValue(value)
+        })
+        result = result + " ]"
+        return result;
+    }
+
+    let printNetwork = (network) => {
+        const indentation = "  "
+        let result = "{\n"
+        network.network.forEach(statement => {
+            console.log(statement)
+                    result = result + indentation + printValue(statement[0]) + " " + printValue(statement[1]) + " " + printValue(statement[2]) + "\n"
+                })
+        result = result + "}\n"
+        return result;
+    }
+
+    engine.addListener((state) => {
         if (state.name != undefined) {
             result = ""
-            result = "Partial Result = " + printValue(partialResult) + "\n";
+            result = "Partial Result = " + printValue(state.partialResult) + "\n";
             result = result + "Current Network = @" + state.name + "\n"
             state.networks.forEach(network => {
-                result = result + "  @" + network.name + "\n"
-                network.network.forEach(statement => {
-                    result = result + "    " + printValue(statement[0]) + " " + printValue(statement[1]) + " " + printValue(statement[2]) + "\n"
-                })
+                result = result + "@" + network.name + " "
+                result = result + printNetwork(network)
             })
         } else {
             result = state.UserMessage
